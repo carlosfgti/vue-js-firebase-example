411cd9671d42d2d903de65e24b977039cabee0eb
Carlos Ferreira <conta@especializati.com.br>
2019-06-20 15:27:13 -0300

    Register Firebase Done

--- src/views/Auth/Register.vue
+++ src/views/Auth/Register.vue
@@ -3,9 +3,9 @@
         <h1 class="title">Cadastro</h1>
 
         <section>
-            <b-field label="Nome">
+            <!-- <b-field label="Nome">
                 <b-input v-model="formData.name"></b-input>
-            </b-field>
+            </b-field> -->
             <b-field label="E-mail">
                 <b-input v-model="formData.email"></b-input>
             </b-field>
@@ -17,27 +17,50 @@
                 </b-input>
             </b-field>
 
-            <b-button
-                type="is-success"
-                outlined
-                :loading="loading">
+            <button
+                :class="['button', 'is-success', 'is-outlined', {'is-loading': loading}]"
+                @click="register">
                 Cadastrar
-            </b-button>
+            </button>
         </section>
     </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
     name: 'register',
     data () {
         return {
             loading: false,
             formData: {
-                name: '',
+                // name: '',
                 email: '',
                 password: '',
             }
+        }
+    },
+
+    methods: {
+        register () {
+            this.loading = true
+
+            // console.log({...this.formData})
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(this.formData.email, this.formData.password)
+                .then(
+                    (user) => {
+                        console.log(user)
+
+                        this.$router.push({name: 'chat'})
+                    },
+                    (err) => {
+                        console.log(`Ops... ${err.message}`)
+                    }
+                )
+                .finally(() => this.loading = false)
         }
     }
 }
