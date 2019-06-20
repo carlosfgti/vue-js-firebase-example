fb6c24178e665f9996c112bd7da53c825cb853ee
Carlos Ferreira <conta@especializati.com.br>
2019-06-20 15:30:18 -0300

    Login Done

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,5 +1,60 @@
 <template>
     <div>
-        Login
+        <h1 class="title">Login</h1>
+
+        <section>
+            <b-field label="E-mail">
+                <b-input v-model="email"></b-input>
+            </b-field>
+            <b-field label="Senha">
+                <b-input
+                    type="password"
+                    v-model="password"
+                    password-reveal>
+                </b-input>
+            </b-field>
+
+            <button
+                :class="['button', 'is-success', 'is-outlined', {'is-loading': loading}]"
+                @click="login">
+                Entrar
+            </button>
+        </section>
     </div>
 </template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+    name: 'login',
+    data () {
+        return {
+            loading: false,
+            email: '',
+            password: '',
+        }
+    },
+
+    methods: {
+        login () {
+            this.loading = true
+
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(this.email, this.password)
+                .then(
+                    (user) => {
+                        console.log(user)
+
+                        this.$router.push({name: 'chat'})
+                    },
+                    (err) => {
+                        console.log(`Ops... ${err.message}`)
+                    }
+                )
+                .finally(() => this.loading = false)
+        }
+    }
+}
+</script>
